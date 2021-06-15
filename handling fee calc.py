b5ddc3bddbf989e800376b69f7f6c25684aa7d59
rexy66 <80185272+rexy66@users.noreply.github.com>
2021-06-15 10:31:08 +0800

    Update handling fee calc.py
    Put calculation code into a function

--- handling fee calc.py
+++ handling fee calc.py
@@ -7,20 +7,29 @@ import math
 code = int(input("What stock are you trading?"))
 lot_size = int(input("How many shares in one lot?"))
 lots = int(input("How many lots are you trading?"))
-pur_price=float(input("The purchase price?"))
-cost = lot_size * pur_price * lots
-print (f"cost is : {cost}")
+pur_price = float(input("The purchase price?"))
+purcost = lot_size * pur_price * lots
 
-brokeragefee = float(cost * 0.0025)
-if brokeragefee > 100:
-    brokeragefee = 100
 
-stampduty = math.ceil(cost * 0.001)
-translevy = math.ceil(cost * 0.000027)
-hkexfee = math.ceil(cost * 0.00005)
 
-print (f"Brokerage fee: {brokeragefee}")
-print (f"Stamp duty: {stampduty}")
-print (f"Transaction levy:{translevy}")
-print (f"Hkex fee: {hkexfee}")
-print (f"total transaction fee")
+
+def calhandlingfee(cost):
+    brokeragefee = float(cost * 0.0025)
+    if brokeragefee > 100:
+        brokeragefee = 100
+    if brokeragefee < 2500:
+        brokeragefee = 2500
+
+    stampduty = math.ceil(cost * 0.001)
+    translevy = math.ceil(cost * 0.000027)
+    hkexfee = math.ceil(cost * 0.00005)
+    totalhandlfee = brokeragefee + stampduty + translevy + hkexfee
+
+    print (f"Cost of purchase is : {cost}")
+    print (f"Brokerage fee: {brokeragefee}")
+    print (f"Stamp duty: {stampduty}")
+    print (f"Transaction levy:{translevy}")
+    print (f"Hkex fee: {hkexfee}")
+    print (f"Total transaction fee: {totalhandlfee}")
+
+calhandlingfee(purcost)
