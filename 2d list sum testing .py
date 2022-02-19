6c80e92a804bc630e25f83d607f75c041cae4697
rexy66 <80185272+rexy66@users.noreply.github.com>
2022-02-19 17:35:56 +0800

    try to do it the no numnpyway

--- 2d list sum testing .py
+++ 2d list sum testing .py
@@ -9,3 +9,20 @@ total = sum(portfolio[:, 0])
 print (portfolio)
 print(total)
   
+
+portfolio = [
+    [1,2],
+    [3,4]
+]
+
+for i in portfolio:
+    print([0])
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+print(number_grid[2][2])
