b20304c7283544bad354519e0af9c7f8ed736581
rexy66 <80185272+rexy66@users.noreply.github.com>
2022-02-27 12:10:42 +0800

    Update and rename Exp3.py to 2d list sum (no numpy).py

---
+++ 2d list sum (no numpy).py
@@ -0,0 +1,16 @@
+
+
+portfolio = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+def sumColumn(m, column):
+    total = 0
+    for row in range(len(m)):
+        total += m[row][column]
+    return total
+
+column = 1
+print("Sum of the elements in column", column, "is", sumColumn(portfolio, column))
