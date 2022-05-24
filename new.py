e1714faae6d6f9d70dad2cf0a01bc92d0e4cd884
rexy66 <80185272+rexy66@users.noreply.github.com>
2022-05-24 13:22:53 +0800

    ethermine.py
    is now ethermineapi.py

--- new.py
+++ new.py
@@ -1,1 +1,47 @@
+import requests 
+import time
+# miner addess 0xe14e6353c15412C6aB825Ef91c85E7aa904802dC
 
+ming = 'https://api.ethermine.org/miner/0xe14e6353c15412C6aB825Ef91c85E7aa904802dC/currentStats'
+canus = 'https://api.ethermine.org/miner/0x874b12d3a324088A37F5C48467f74c5121106D12/currentStats'
+mingdefault=3
+canusdefault =3
+diff= True
+displayed = False
+
+def checkworkers(link):
+    req = requests.get(link)
+    result = req.text
+    result = result.replace("{","")
+    result = result.replace("}","")
+    result = result.replace("\"","")
+    result = result.replace(":",",")
+    result = list(result.split(","))
+    index = result.index("activeWorkers")
+    end_index = index + 1
+    end_result = result[end_index]
+    return end_result
+
+
+def checkdiff(user,default):
+    if checkworkers(user) != default:
+        diff = True
+        diff= True
+        displayed = False
+    else:
+        diff = False
+
+    
+while True:
+    checkdiff(ming,mingdefault)
+    checkdiff(canus,canusdefault)
+    time.sleep(10)
+    if diff and not displayed:
+        displayed = True
+        print("The number of workers for Ming is now: " + str(checkworkers(ming)))
+        print("The number of workers for Canus is now: " + str(checkworkers(canus)))
+
+    else:
+        continue
+    
+    
